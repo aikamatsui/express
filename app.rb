50006e5ad341ef98b33540983c1b14cca7446c5b
aikamatsui <aikamm777@gmail.com>
2020-06-18 23:06:18 +0900

    update app.rb

--- app.rb
+++ app.rb
@@ -1,66 +1,66 @@
-require 'bundler/setup'
-Bundler.require
-require 'sinatra/reloader' if development?
-require 'open-uri'
-require 'json'
-require 'net/http'
+  require 'bundler/setup'
+  Bundler.require
+  require 'sinatra/reloader' if development?
+  require 'open-uri'
+  require 'json'
+  require 'net/http'
 
-require 'sinatra/activerecord'
-require './models'
+  require 'sinatra/activerecord'
+  require './models'
 
-get '/' do
-  @histories = History.all.limit(10).order("created_at desc")
-  @favorites = History.where(favorite: true)
-  erb :form
-end
-
-get '/list' do
-  @x = params[:x]
-  @y = params[:y]
-  History.create!(x: @x, y: @y)
-
-  uri = URI("http://express.heartrails.com/api/json")
-  uri.query = URI.encode_www_form({
-    method: "getStations",
-    x: @x,
-    y: @y
-  })
-
-  res = Net::HTTP.get_response(uri)
-  json = JSON.parse(res.body)
-  @stations = json["response"]["station"]
-  erb :list
-
-end
-
-get '/api/station' do
-  uri = URI("http://express.heartrails.com/api/json")
-  uri.query = URI.encode_www_form({
-    method: "getStations",
-    line: params[:line],
-    name: params[:name]
-  })
-  res = Net::HTTP.get_response(uri)
-  json = JSON.parse(res.body)
-  if json["response"]["error"]
-  response = {error: "No Station."}
-  else
-    response = {
-      next: json["response"]["station"][0]["next"]
-    }
+  get '/' do
+    @histories = History.all.limit(10).order("created_at desc")
+    @favorites = History.where(favorite: true)
+    erb :form
   end
-  json response
-end
 
-post '/:id/delate' do
-  history = History.find(params[:id])
-  history.delete
-  redirect "/"
-end
+  get '/list' do
+    @x = params[:x]
+    @y = params[:y]
+    History.create!(x: @x, y: @y)
 
-post '/:id/update' do
-  history = History.find(params[:id])
-  history.favorite = !history.favorite
-  history.save
-  redirect"/"
-end
+    uri = URI("http://express.heartrails.com/api/json")
+    uri.query = URI.encode_www_form({
+      method: "getStations",
+      x: @x,
+      y: @y
+    })
+
+    res = Net::HTTP.get_response(uri)
+    json = JSON.parse(res.body)
+    @stations = json["response"]["station"]
+    erb :list
+
+  end
+
+  get '/api/station' do
+    uri = URI("http://express.heartrails.com/api/json")
+    uri.query = URI.encode_www_form({
+      method: "getStations",
+      line: params[:line],
+      name: params[:name]
+    })
+    res = Net::HTTP.get_response(uri)
+    json = JSON.parse(res.body)
+    if json["response"]["error"]
+    response = {error: "No Station."}
+    else
+      response = {
+        next: json["response"]["station"][0]["next"]
+      }
+    end
+    json response
+  end
+
+  post '/:id/delate' do
+    history = History.find(params[:id])
+    history.delete
+    redirect "/"
+  end
+
+  post '/:id/update' do
+    history = History.find(params[:id])
+    history.favorite = !history.favorite
+    history.save
+    redirect"/"
+  end
